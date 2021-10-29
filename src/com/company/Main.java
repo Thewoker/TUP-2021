c44a3cb76e99144d08afd3c0e78723aeb86345da
RomArgentino <78028834+RomArgentino@users.noreply.github.com>
2021-10-29 11:26:44 -0300

    Listado recursivo

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,13 +6,22 @@ public class Main {
 
     public static void main(String[] args) {
 
-        File archivo = new File("C:" + File.separator + "Users" + File.separator + "Usuario" + File.separator + "Desktop" + File.separator + "TUP-2021");
+        File archivo = new File("C:"+ File.separator+ "Users"+ File.separator+"roma_"+ File.separator+ "desktop" + File.separator + "TUP-2021");
 
         String[] nombres = archivo.list();
 
         for (int i = 0; i < nombres.length; i++){
             System.out.println(nombres[i]);
+            File archivo2 = new File(archivo.getAbsolutePath(),nombres[i]);
+            if(archivo2.isDirectory()){
+                String [] subCarpetas = archivo2.list();
+                for (int j = 0; j < subCarpetas.length; j++){
+                    System.out.println("\t |__"+subCarpetas[j]);
+                }
+            }
         }
+
+
 
     }
 }
